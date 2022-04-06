21aa2415d4eb2b82d3c9c009ae8256db83c8b488
noahliebs <noah.s.lieberman@gmail.com>
2022-04-05 17:35:25 -0700

    fix bug

--- static/jquery.js
+++ static/jquery.js
@@ -11,3 +11,27 @@ function toggleInstructions(id) {
     var div = document.getElementById(id);
     div.style.display = div.style.display == "none" ? "block" : "none";
 }
+
+function getTimezoneName() {
+  const today = new Date();
+  const short = today.toLocaleDateString(undefined);
+  const full = today.toLocaleDateString(undefined, { timeZoneName: 'long' });
+
+  // Trying to remove date from the string in a locale-agnostic way
+  const shortIndex = full.indexOf(short);
+  if (shortIndex >= 0) {
+    const trimmed = full.substring(0, shortIndex) + full.substring(shortIndex + short.length);
+    
+    // by this time `trimmed` should be the timezone's name with some punctuation -
+    // trim it from both sides
+    return trimmed.replace(/^[\s,.\-:;]+|[\s,.\-:;]+$/g, '');
+
+  } else {
+    // in some magic case when short representation of date is not present in the long one, just return the long one as a fallback, since it should contain the timezone's name
+    return full;
+  }
+}
+
+
+var myTable = document.getElementById('statsTable');
+myTable.rows[0].cells[1].innerHTML = 'Hello';
